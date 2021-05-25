dd24df88e39b19680300fb5d7db53162f7c0f414
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2021-05-25 19:04:14 -0300

    commenting code

--- geo-database/sample-assessment/app.r
+++ geo-database/sample-assessment/app.r
@@ -1,13 +1,24 @@
 library(shiny)
+library(shinythemes)
+library(sparkline)
+library(shinycssloaders)
 library(sits)
 library(dplyr)
 library(ggplot2)
 library(sf)
 
 ui <- fluidPage(
-  titlePanel("Quality control samples - Assess Quality"),
+  theme = shinytheme("cerulean"),
+  tags$head(
+    tags$link(href = "style.css", rel = "stylesheet")
+  ),
+  titlePanel(
+    "Quality control samples - Assess Quality",
+    windowTitle = "Quality control samples - Assess Quality"
+  ),
   sidebarLayout(
     sidebarPanel(
+      width = 3,
       fileInput("tb_file", "Choose Tibble File", accept = ".rds"),
       selectInput("select", "Select band", choices = list("NDVI"= 'NDVI'), selected = 1),
       hr(),
@@ -16,14 +27,29 @@ ui <- fluidPage(
       fluidRow(column(10, verbatimTextOutput("bands"))),
     ),
     mainPanel(
-      plotOutput("ts_plot"),
-      br(), br(),
-      plotOutput("som_plot"),
+      width = 7,
+      tabsetPanel(
+        tabPanel(
+          div("Medical History"),
+          wellPanel(h3("Medical History and Tests Timeline")),
+          plotOutput("ts_plot"),
+          br(), br(),
+          plotOutput("som_plot"),
+        ),
+        tabPanel(
+          div("Medical History"),
+          wellPanel(
+            h3("Vets"),
+            wellPanel(h3("Medical History and Tests Timeline")),
+            plotOutput("ts_plot"),
+            br(), br(),
+            plotOutput("som_plot")
+          )
+        )
+      )
     )
   )
 )
-
-input_data.tb
 
 server <- function(input, output) {
   output$value <- renderPrint({ input$select })
@@ -55,6 +81,7 @@ server <- function(input, output) {
     ext <- tools::file_ext(file$datapath)
     req(file)
     validate(need(ext == "rds", "Please upload a RDS file..."))
+    input_data.tb <- readRDS(file$datapath)
     set.seed(777)
     clustering_CB4_workshop.lst <- sits::sits_som_map(
       input_data.tb,
