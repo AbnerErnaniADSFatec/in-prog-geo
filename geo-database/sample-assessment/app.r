b89add26355ec6065467f3b8bed97b92753e014e
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2021-05-26 17:27:38 -0300

    working on geo db project

--- geo-database/sample-assessment/app.r
+++ geo-database/sample-assessment/app.r
@@ -1,49 +1,63 @@
 library(shiny)
 library(shinythemes)
-library(sparkline)
 library(shinycssloaders)
 library(sits)
-library(dplyr)
 library(ggplot2)
 library(sf)
 
 ui <- fluidPage(
   theme = shinytheme("cerulean"),
   tags$head(
-    tags$link(href = "style.css", rel = "stylesheet")
-  ),
-  titlePanel(
-    "Quality control samples - Assess Quality",
-    windowTitle = "Quality control samples - Assess Quality"
-  ),
-  sidebarLayout(
-    sidebarPanel(
-      width = 3,
-      fileInput("tb_file", "Choose Tibble File", accept = ".rds"),
-      selectInput("select", "Select band", choices = list("NDVI"= 'NDVI'), selected = 1),
-      hr(),
-      fluidRow(column(10, verbatimTextOutput("render_tb"))),
-      hr(),
-      fluidRow(column(10, verbatimTextOutput("bands"))),
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+    tags$title("Quality control samples"),
+    titlePanel(
+      "Quality control samples",
+      windowTitle = "Quality control samples - Assess Quality"
     ),
-    mainPanel(
-      width = 7,
-      tabsetPanel(
-        tabPanel(
-          div("Medical History"),
-          wellPanel(h3("Medical History and Tests Timeline")),
-          plotOutput("ts_plot"),
-          br(), br(),
-          plotOutput("som_plot"),
-        ),
-        tabPanel(
-          div("Medical History"),
-          wellPanel(
-            h3("Vets"),
-            wellPanel(h3("Medical History and Tests Timeline")),
-            plotOutput("ts_plot"),
-            br(), br(),
-            plotOutput("som_plot")
+  ),
+  tags$body(
+    navbarPage(
+      title="My Application",
+      tabPanel("Component 1"),
+      tabPanel("Component 2"),
+      tabPanel("Component 3")
+    ),
+    sidebarLayout(
+      sidebarPanel(
+        width = 3,
+        fileInput("tb_file", "Choose Tibble File", accept = ".rds"),
+        selectInput("select", "Select band", choices = list("NDVI"= 'NDVI'), selected = 1),
+        hr(),
+        fluidRow(column(10, verbatimTextOutput("bands"))),
+      ),
+      mainPanel(
+        class = "mainPanel",
+        tabsetPanel(
+          tabPanel(
+            div("Assess Quality"),
+            wellPanel(
+              div(
+                class="som_grid",
+                checked=NA,
+                h3("Time Series Plot"),
+                plotOutput("ts_plot"),
+                br(), br(),
+                h3("SOM Grid"),
+                plotOutput("som_plot")
+              )
+            )
+          ),
+          tabPanel(
+            div("Data Table"),
+            wellPanel(
+              div(
+                class="som_grid",
+                checked=NA,
+                h3("Data Tibble File"),
+                hr(),
+                fluidRow(column(10, verbatimTextOutput("render_tb"))),
+              )
+            )
           )
         )
       )
@@ -69,12 +83,22 @@ server <- function(input, output) {
     readRDS(file$datapath)
   })
   output$ts_plot <- renderPlot({
-    file <- input$tb_file
-    ext <- tools::file_ext(file$datapath)
-    req(file)
-    validate(need(ext == "rds", "Please upload a RDS file..."))
-    input_data.tb <- readRDS(file$datapath)
-    plot(sits_select(input_data.tb, bands = input$select))
+    withProgress(message = 'Making plot', value = 0, {
+      
+      incProgress(1/4, detail = paste("Reading file", 1))
+      file <- input$tb_file
+      ext <- tools::file_ext(file$datapath)
+      req(file)
+      
+      incProgress(1/4, detail = paste("Validating File", 2))
+      validate(need(ext == "rds", "Please upload a RDS file..."))
+      
+      incProgress(1/4, detail = paste("Parsing RDS File", 3))
+      input_data.tb <- readRDS(file$datapath)
+      plot(sits_select(input_data.tb, bands = input$select))
+      
+      incProgress(1/4, detail = paste("Plotting File", 4))
+    })
   })
   output$som_plot <- renderPlot({
     file <- input$tb_file
