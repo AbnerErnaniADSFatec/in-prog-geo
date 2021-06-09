b9a411a0035c7b32151867ce6b296f50d93170f9
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2021-06-09 14:46:26 -0300

    adding documentation

--- prog-geo/api-eodc-testes/eocube/build.sh
+++ prog-geo/api-eodc-testes/eocube/build.sh
@@ -4,3 +4,4 @@ check-manifest && \
 pytest && \
 sphinx-build -qnW --color -b doctest help/source help/_build && \
 cd help && sphinx-build -b html -d _build/doctrees source build
+mv ./build/ ../../../../docs/
