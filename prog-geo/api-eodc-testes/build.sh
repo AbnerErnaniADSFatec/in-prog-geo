18ce6b07c33d0cabab36f1e56ea143c1b4466fd4
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2021-06-11 17:54:27 -0300

    transfering eocubes to oficial repo

--- prog-geo/api-eodc-testes/build.sh
+++
@@ -1,7 +0,0 @@
-pydocstyle eocube/*.py eocube-server/*.py && \
-isort eocube eocube/setup.py eocube-server/setup.py --check-only --diff && \
-check-manifest eocube-server && \
-check-manifest eocube && \
-pytest && \
-sphinx-build -qnW --color -b doctest help/source help/_build && \
-cd help && sphinx-build -b html -d _build/doctrees source build
